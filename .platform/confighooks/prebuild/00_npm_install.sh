76c601b9bf21595cf3c718f800b5da65455a838d
DJCrossman <david@crossman.io>
2024-01-10 14:06:50 -0600

    chore: cache sharp and pm2 npm installations to speed up deploys

--- .platform/confighooks/prebuild/00_npm_install.sh
+++ .platform/confighooks/prebuild/00_npm_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /var/app/staging
-sudo -u webapp npm install sharp --omit=dev
-sudo -u webapp npm install pm2
+# write if statement to check if node_modules exists else run npm install
+if [ -d "/tmp/node_modules" ]; then
+  sudo -u webapp mkdir -p /var/app/staging/node_modules
+  sudo -u webapp cp -r /tmp/node_modules/* /var/app/staging/node_modules
+  sudo -u webapp cp -r /tmp/node_modules/.bin /var/app/staging/node_modules/.bin
+else
+  sudo -u webapp npm install sharp --omit=dev
+  sudo -u webapp npm install pm2
+  sudo -u webapp mkdir -p /tmp/node_modules
+  sudo -u webapp cp -r /var/app/staging/node_modules/* /tmp/node_modules/
+  sudo -u webapp cp -r /var/app/staging/node_modules/.bin /tmp/node_modules/.bin
+fi
